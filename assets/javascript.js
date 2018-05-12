e57d4fe4587c23c9cd71d5e90000a197a7322e6b
gwapesofwath <gwapesofwath@gmail.com>
2018-05-12 02:12:25 -0500

    next commit

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,5 @@
 //inital array of basketball topics
-var topics = ["Shaquille O'Neal", "Buzzer Beater", "Charles Barkley", "Basketball Fans", "Kobe", "90s Basketball", "Dunks", "White Chocolate", "Lebron James", "Gregg Popovich", "Basketball Cartoon"];
+var topics = ["Tim Duncan", "Buzzer Beater", "Charles Barkley", "Basketball Fans", "Kobe", "90s Basketball", "Dunks", "Shaquille O'Neal", "White Chocolate", "Lebron James", "Gregg Popovich", "Basketball Cartoon", "Shawn Kemp", "Russel Westbrook", "Metta World Peace", "Reggie Miller", "Swaggy P"];
 
 //display buttons of basketball topics with for loop
 function createButtons() {
@@ -18,28 +18,45 @@ createButtons();
 
 //on click event function for each unique button. When button is clicked 
 //ajax get method is called and a promise is returned
-$(document).on("click",".basketballTings", function() {
+$("#newButtons").on("click",".basketballTings", function() {
 
-    for (var g = 0; g < 10; g++ ) {
+    // for (var g = 0; g < 10; g++ ) {
 
-    var topic = $(this).attr("data-name");
-    
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=ShzYiNXVS6tBkqGxnjhi13ylw1ARwsOU&limit=10&tag=" + topic;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        var gifUrl = response.data.images.downsized_medium.url;
-        console.log(response);
-        var newGif = $("<img>");
+        var topic = $(this).attr("data-name");
+        
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=ShzYiNXVS6tBkqGxnjhi13ylw1ARwsOU&limit=10";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            $("#basketballGifs").empty();  
+            console.log(response.data[0]);         
+            for (let g = 0; g < response.data.length; g++) {
+                let gifUrl = response.data[g].images.downsized_medium.url;
+                let stillUrl = response.data[g].images["480w_still"].url;
+                let rating = response.data[g].rating;
+                let newGif = $("<img>");
+                newGif.addClass("gifImage");
+                newGif.attr("data-stillUrl", stillUrl);
+                newGif.attr("data-gifUrl", gifUrl)
+                newGif.attr("src", stillUrl);
+                newGif.attr("alt", "basketball gif");
 
-        newGif.attr("src", gifUrl);
-        newGif.attr("alt", "basketball gif");
-        $("#basketballGifs").prepend(newGif);
-    
+                $("#basketballGifs").prepend(newGif,rating);
+            } 
 
-    })
+        })
+    // }
+})
+
+//on click event function for animating gifs
+$("#basketballGifs").on("click",".gifImage", function() {
+    var gifUrl = $(this).attr("data-gifUrl");
+    var stillUrl = $(this).attr("data-stillUrl");
+    if ($(this).attr("src")===stillUrl){
+        $(this).attr("src", gifUrl)
     }
+    else ($(this).attr("src", stillUrl))
 })
 
 //on click event function for adding a new buton
